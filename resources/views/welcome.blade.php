b2aa4ae12e99ad2ce169131fa51b457041e99d82
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-06-29 17:53:13 +0200

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                   <h5 class="card-title">{{$train ->azienda}}</h5>
                   <h6 class="card-subtitle mb-2 text-muted">Da: {{$train ->stazione_partenza}}</h6>
-                  <p class="card-text">Arrivo: {{$train ->stazione_arrivo}}</p>
+                  <h6 class="card-subtitle mb-2 text-muted">Arrivo: {{$train ->stazione_arrivo}}</h6>
                   <p class="card-text">OrarioP: {{$train ->orario_partenza}}</p>
                   <p class="card-text">OrarioA: {{$train ->orario_arrivo}}</p>
                   <p class="card-text">CD Train: {{$train ->codice_treno}}</p>
